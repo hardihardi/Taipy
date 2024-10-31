27aeecb2d079d7fad7b6220662a5bd5adc1db7a8
SGCODEX <64886313+SGCODEX@users.noreply.github.com>
2024-10-31 12:01:24 +0530

    Updated markdown example

---
+++ doc/gui/examples/matplotlib/markdown_matplotlib_example.py
@@ -0,0 +1,25 @@
+#Using markdown syntax
+import matplotlib.pyplot as plt
+import numpy as np
+
+from taipy.gui import Gui,Markdown
+
+fig = plt.figure(figsize=(5,4))
+xx = np.arange(0, 2 * np.pi, 0.01)
+plot = fig.subplots(1, 1)
+plot.fill(xx, np.sin(xx), facecolor="none", edgecolor="purple", linewidth=2)
+
+# Define a page using Markdown syntax
+page_content = Markdown("""
+# Taipy Example for Matplotlib Integration using Markdown Syntax
+<|part|content={fig}|class_name=matplotlib_example|>
+""",style={
+    ".matplotlib_example": {
+        "display": "inline-flex",
+        "width": "520px",
+        "height": "420px"
+    }}
+)
+
+if __name__ == "__main__":
+    Gui(page_content).run(title="Matplotlib Example")
