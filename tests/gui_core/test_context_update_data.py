c50ffa0d90b784e41a50b92cbd24aba1ba81191d
jean-robin medori <jeanrobin.medori@avaiga.com>
2024-11-25 10:36:28 +0100

    Fix wrong import

--- tests/gui_core/test_context_update_data.py
+++ tests/gui_core/test_context_update_data.py
@@ -8,11 +8,10 @@
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
-
+from datetime import datetime
 from unittest.mock import Mock, patch
 
 import pytest
-from win32ctypes.pywin32.pywintypes import datetime
 
 from taipy import DataNode, Gui
 from taipy.common.config.common.scope import Scope
