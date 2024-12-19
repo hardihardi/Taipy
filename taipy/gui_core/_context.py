6d1db45ff03357b9fba4f0d9b65314b030826b32
Fred Lefévère-Laoide <Fred.Lefevere-Laoide@Taipy.io>
2024-12-19 09:10:40 +0100

    readable submission auth resolves #562

--- taipy/gui_core/_context.py
+++ taipy/gui_core/_context.py
@@ -155,7 +155,7 @@ class _GuiCoreContext(CoreEventConsumerBase):
         self.broadcast_core_changed({"scenario": scenario_id or True})
 
     def submission_status_callback(self, submission_id: t.Optional[str] = None, event: t.Optional[Event] = None):
-        if not submission_id or not is_readable(t.cast(SubmissionId, submission_id)):
+        if not submission_id:
             return
         submission = None
         new_status: t.Optional[SubmissionStatus] = None
